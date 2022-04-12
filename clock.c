7c514e8875845f87962170b5fae8c6361985a71c
zione-kushwaha <jeevankushwaha461@gmail.com>
2022-04-12 17:04:32 +0545

    init

---
+++ clock.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <time.h> //for sleep() function
+#include <unistd.h>
+#include <stdlib.h>
+ 
+int main()
+{
+    int hour, minute, second;
+     
+   printf("enter the hour:");
+   scanf("%d",&hour);
+   printf("enter the minute:");
+   scanf("%d",&minute);
+   printf("enter the second:");
+   scanf("%d",&second);
+ 
+    while(1)
+    {
+        //clear output screen
+        system("cls");
+         
+        //print time in HH : MM : SS format
+        printf("%02d : %02d : %02d ",hour,minute,second);
+         
+         //clear output buffer in gcc
+        fflush(stdout);
+         
+         //increase second
+        second++;
+ 
+        //update hour, minute and second
+        if(second==60){
+            minute+=1;
+            second=0;
+        }
+        if(minute==60){
+            hour+=1;
+            minute=0;
+        }
+        if(hour==24){
+            hour=0;
+            minute=0;
+            second=0;
+        }
+         
+        sleep(1);   //wait till 1 second
+    }
+ 
+    return 0;
+}
